d1f5f2262bf6ee8afb615294542cb9b61418acf4
SoloGudLyf <solomonteiko0@gmail.com>
2025-10-09 00:18:21 +0000

    Allow user to type in expressions and evaluate them.

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 let firstNum;
 let operator;
 let secondNum;
+const container = document.querySelector(".container");
+const displayArea = document.querySelector(".display");
+const equalTo = document.querySelector(".equalTo");
 
 function add(arr) {
-  return arr.reduce((acc, curr) => (acc += curr), 0);
+  return arr[0] + arr[1];
 }
 
 function subtract(arr) {
-  return arr.reduce((acc, curr) => (acc -= curr), 0);
+  return arr[0] - arr[1];
 }
 
 function multiply(arr) {
-  return arr.reduce((acc, curr) => (acc *= curr), 1);
+  return arr[0] * arr[1];
 }
 
 function divide(arr) {
-    arr.reverse()
-  return arr.reduce((acc, curr) =>acc = curr / acc, 1);
+    arr[0] / arr[1]
 }
 function operate(num1, symbol, num2) {
   if (symbol === "+") return add([num1, num2]);
@@ -25,6 +27,38 @@ function operate(num1, symbol, num2) {
   if (symbol === "÷") return divide([num1, num2]);
 }
 
+function display(event) {
+  let elemClicked = event.target;
+
+  if (elemClicked.textContent === "C") {
+    displayArea.textContent = "";
+  } else if (elemClicked.textContent === "x") {
+    let exp = Array.from(displayArea.textContent);
+    exp.pop();
+    console.log(exp);
+    displayArea.textContent = exp.join("");
+  } else if (elemClicked.textContent === "=") {
+  } else {
+    displayArea.textContent += elemClicked.textContent;
+  }
+}
+
+function evaluate(event) {
+  let elemClicked = event.target;
+  let operator;
+  if (displayArea.textContent.includes("+")) operator = "+";
+  if (displayArea.textContent.includes("-")) operator = "-";
+  if (displayArea.textContent.includes("×")) operator = "×";
+  if (displayArea.textContent.includes("÷")) operator = "÷";
+  let expArr = displayArea.textContent.split(operator);
+  console.log(expArr);
+  firstNum = Number(expArr[0]);
+  secondNum = Number(expArr[1]);
+  displayArea.textContent = operate(firstNum, operator, secondNum);
+}
+
+container.addEventListener("click", display);
+equalTo.addEventListener("click", evaluate);
 console.log(operate(1, "+", 5));
 console.log(operate(1, "-", 9));
 console.log(operate(1, "×", 189));
