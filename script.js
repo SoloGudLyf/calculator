bce89e8527fd29565e51e80bb7f394cebc594b21
SoloGudlyf <solomonteiko0@gmail.com>
2026-06-05 13:44:21 +0000

    fix: Avoid repeated dots in numbers and round results

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const container = document.querySelector(".container");
 const displayArea = document.querySelector(".display");
 container.addEventListener("click", updateDisplayArea);
 let operatorCount = 0;
+let leadingOperator = false;
 function add(arr) {
   return arr[0] + arr[1];
 }
@@ -21,21 +22,46 @@ function divide(arr) {
   return arr[0] / arr[1];
 }
 function operate(num1, symbol, num2) {
-  if (symbol === "+") return add([num1, num2]);
-  if (symbol === "-") return subtract([num1, num2]);
-  if (symbol === "×") return multiply([num1, num2]);
-  if (symbol === "÷") return divide([num1, num2]);
+  let result;
+  if (symbol === "+") {
+    result = add([num1, num2]);
+  } else if (symbol === "-") {
+    result = subtract([num1, num2]);
+  } else if (symbol === "×") {
+    result = multiply([num1, num2]);
+  } else if (symbol === "÷") {
+    result = divide([num1, num2]);
+  }
+  if (isNaN(result) || result === Infinity) {
+    alert("Math error, check computation");
+    result = 0;
+  } else if (result % 1 != 0) return result.toFixed(5);
+
+  return result;
 }
 
 function updateDisplayArea(event) {
   let elemClicked = event.target;
   let exp = Array.from(displayArea.textContent);
-  console.log(exp);
+  let dotInExpCount = 0;
+  exp.map((elem) => {
+    if (elem === ".") dotInExpCount++;
+  });
 
+  //Clear display area
   if (elemClicked.textContent === "C") {
     displayArea.textContent = 0;
-  }
-
+  } else if (
+    elemClicked.className === "display" ||
+    elemClicked.textContent.length > 3
+  )
+    return;
+  else if (
+    exp.length > 18 &&
+    elemClicked.textContent != "=" &&
+    elemClicked.textContent != "del"
+  )
+    return;
   // Delete user input
   else if (elemClicked.textContent === "del") {
     exp.pop();
@@ -53,14 +79,30 @@ function updateDisplayArea(event) {
     elemClicked.textContent === "÷"
   ) {
     // Check if an operator already exists
-    if (checkOperator()) {
+    if (checkOperator(Array.from(displayArea.textContent))) {
       return;
     } else {
       displayArea.textContent += elemClicked.textContent;
     }
   }
   // Prevent double decimals in a number
-  else if (exp.includes(".") && elemClicked.textContent === ".") return;
+  // return if there's a point in the first number
+  else if (
+    exp.includes(".") &&
+    elemClicked.textContent === "." &&
+    dotInExpCount <= 1 &&
+    !checkOperator(exp.slice(1))
+  ) {
+    return;
+  }
+  // return if there's a point in the second number
+  else if (
+    checkOperator(exp.slice(1)) &&
+    dotInExpCount >= 2 &&
+    elemClicked.textContent === "."
+  ) {
+    return;
+  }
   // Display number buttons clicked or decimal
   else {
     displayArea.textContent =
@@ -70,10 +112,9 @@ function updateDisplayArea(event) {
   }
 }
 
-function checkOperator() {
-  let exp = Array.from(displayArea.textContent);
-
+function checkOperator(exp) {
   if (exp[0] === "-" || exp[0] === "+") {
+    leadingOperator = true;
     // Check if there are other operators clicked
     let operators = ["+", "-", "×", "÷"];
     operatorCount = 0;
@@ -95,7 +136,7 @@ function checkOperator() {
 }
 
 function evaluate() {
-  if (!checkOperator()) {
+  if (!checkOperator(Array.from(displayArea.textContent))) {
     return;
   }
 
@@ -106,15 +147,10 @@ function evaluate() {
   if (operatorCount === 2) {
     let secondOperator = getOperator(expArr[1]) || "-";
 
-    console.log(secondOperator, operator);
-    console.log(expArr);
-
     expArr = expArr.concat(expArr[1].split(secondOperator));
-    console.log(expArr);
     firstNum = operator + expArr[2];
     firstNum = Number(firstNum);
     secondNum = Number(expArr[3]);
-    console.log(firstNum, secondNum);
     displayArea.textContent = operate(firstNum, secondOperator, secondNum);
     operatorCount = 0;
   } else {
